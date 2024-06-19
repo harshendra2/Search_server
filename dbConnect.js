0f9b8b75f1e8acb7bb3470e0a8170ce637f88bab
harshendra raj <harsendraraj20@gmail.com>
2024-06-19 09:46:14 +0530

    data base updated

--- dbConnect.js
+++ dbConnect.js
@@ -1,20 +1,27 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 const dbConnect = () => {
-    const connectionParams = { useNewUrlParser: true };
-    mongoose.connect("mongodb+srv://harsendraraj20:gtcJpjF1hebSkrtK@cluster0.8rpiewk.mongodb.net/", connectionParams);
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true
+    };
+
+    mongoose.connect('mongodb+srv://harsendraraj20:gtcJpjF1hebSkrtK@cluster0.8rpiewk.mongodb.net/', connectionParams);
 
     mongoose.connection.on("connected", () => {
-		console.log("Connected to database sucessfully");
-	});
+        console.log("Connected to database successfully");
+    });
 
-	mongoose.connection.on("error", (err) => {
-		console.log("Error while connecting to database :" + err);
-	});
+    mongoose.connection.on("error", (err) => {
+        console.log("Error while connecting to database: " + err);
+    });
 
-	mongoose.connection.on("disconnected", () => {
-		console.log("Mongodb connection disconnected");
-	});
+    mongoose.connection.on("disconnected", () => {
+        console.log("Mongodb connection disconnected");
+    });
 };
 
 module.exports = dbConnect;
+
